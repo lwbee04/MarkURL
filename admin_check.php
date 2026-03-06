74d13f0db97064b739c7a177e89b4c0d6b31c302
lwbee04 <igoygoy957@gmail.com>
2026-03-06 17:42:14 +0700

    Update admin_check.php

--- admin_check.php
+++ admin_check.php
@@ -1,6 +1,10 @@
 <?php
 /**
  * MarkURL - Admin Session Check
+ * FIX: session_start() included for safety
  */
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 $is_admin = isset($_SESSION['admin_logged_in']) && $_SESSION['admin_logged_in'] === true;
 ?>
